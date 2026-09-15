c13fe92a61c13dd7008b556598f85cf1328dc6f3
robert-cronin <robert-cronin@users.noreply.github.com>
2026-09-15 20:07:41 +1000

    Update source formulas to v0.3.6
    Signed-off-by: robert-cronin <robert-cronin@users.noreply.github.com>

--- Formula/flere-connect.rb
+++ Formula/flere-connect.rb
@@ -1,9 +1,9 @@
 class FlereConnect < Formula
   desc "SSH companion for the Flere terminal workbench"
   homepage "https://github.com/robert-cronin/flere"
-  url "https://github.com/robert-cronin/flere/releases/download/v0.3.5/flere-0.3.5-source.tar.gz"
-  version "0.3.5"
-  sha256 "b47e0b741d3e15795a98ff9d107430e94f03d4713e9341d5e7b747f0396142c5"
+  url "https://github.com/robert-cronin/flere/releases/download/v0.3.6/flere-0.3.6-source.tar.gz"
+  version "0.3.6"
+  sha256 "840b62d8106160ef8eba95d244197559b5e5bd203459beea220c202c0d90681c"
   license all_of: ["MIT", "OFL-1.1"]
 
   depends_on "rust" => :build
